cf4592a536d8fb29bff7c1b9f66a54e5b027d2e3
Enggar Ardi Prastiawan <113541769+EnggarAP@users.noreply.github.com>
2024-12-16 20:33:20 +0700

    Update koneksi.js

--- koneksi.js
+++ koneksi.js
@@ -1,17 +1,27 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-//database
 const con = mysql.createConnection({
-  host: process.env.DB_HOST, 
-  user: process.env.DB_USERNAME, 
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DBNAME,
-})
-
-con.connect((err) => {
-  if (err) throw err;
-  console.log('Database terkoneksi');
+  database: process.env.DB_DBNAME, // Ensure this is set correctly
 });
 
-module.exports = con;
+con.connect((err) => {
+  if (err) {
+    console.error('Database connection error:', err.stack);
+    return;
+  }
+  console.log('Database terkoneksi');
+
+  // Test query
+  con.query('SHOW TABLES', (error, results) => {
+    if (error) {
+      console.error('Query error:', error);
+    } else {
+      console.log('Tables:', results);
+    }
+    con.end();
+  });
+});
