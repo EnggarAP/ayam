6182f2493eb256a79a79e2c70fd93df94ff22f60
Enggar Ardi Prastiawan <113541769+EnggarAP@users.noreply.github.com>
2024-12-18 14:06:33 +0700

    update all

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //routers
-const routers = require('.');
+const routers = require('../routers');
 routers(app);
 
 app.listen(3000, () => {
